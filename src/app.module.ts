a2c8df7062b7e2eafbc4d3a0fce8c3fbe153783d
riverandeye <riverandeye@gmail.com>
2020-06-10 17:29:41 +0900

    fix : remove duplicate gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
-import { ChatGateway } from './chat/chat.gateway';
 
 @Module({
   imports: [ChatModule],
   controllers: [AppController],
-  providers: [AppService, ChatGateway],
+  providers: [AppService],
 })
 export class AppModule {}
